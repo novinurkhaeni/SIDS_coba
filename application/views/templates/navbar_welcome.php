88b932261e715eee5b98642b5358b5796db4d3b5
Ridho Aryo Bimo <ridhoaryobimo@gmail.com>
2019-12-09 18:12:30 +0700

    update

--- application/views/templates/navbar_welcome.php
+++ application/views/templates/navbar_welcome.php
@@ -1,11 +1,11 @@
         <!-- Navbar -->
         <nav class="main-header navbar navbar-expand navbar-white navbar-light">
             <!-- Left navbar links -->
-            <!-- <ul class="navbar-nav">
+            <ul class="navbar-nav">
                 <li class="nav-item">
                     <a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
                 </li>
-            </ul> -->
+            </ul>
 
             <form class="form-inline ml-3">
                 <div class="input-group input-group-sm">
