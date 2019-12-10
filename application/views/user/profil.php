7c67925492ea34362b168ec9f8e596d8e77dfd1d
novi <novinurkhaeni.1@gmail.com>
2019-12-10 17:45:59 +0700

    profile

--- application/views/user/profil.php
+++ application/views/user/profil.php
@@ -26,26 +26,12 @@
           <div class="card card-primary card-outline">
             <div class="card-body box-profile">
               <div class="text-center">
-                <img class="profile-user-img img-fluid img-circle" src="<?php echo base_url(); ?>assets/img/foto.jpg" alt="User profile picture">
+                <img class="profile-user-img img-fluid img-circle" src="<?= base_url('assets/img/') . $user['foto']; ?>" alt="User profile picture">
               </div>
 
-              <h3 class="profile-username text-center">Nina Mcintire</h3>
+              <h3 class="profile-username text-center"><?= $user['username'] ?></h3>
 
-              <p class="text-muted text-center">Software Engineer</p>
-
-              <ul class="list-group list-group-unbordered mb-3">
-                <li class="list-group-item">
-                  <b>Followers</b> <a class="float-right">1,322</a>
-                </li>
-                <li class="list-group-item">
-                  <b>Following</b> <a class="float-right">543</a>
-                </li>
-                <li class="list-group-item">
-                  <b>Friends</b> <a class="float-right">13,287</a>
-                </li>
-              </ul>
-
-              <a href="#" class="btn btn-primary btn-block"><b>Follow</b></a>
+              <p class="text-muted text-center">Member Since : <?= date('F Y', $user['tanggal_daftar']); ?></p>
             </div>
             <!-- /.card-body -->
           </div>
@@ -58,35 +44,15 @@
             </div>
             <!-- /.card-header -->
             <div class="card-body">
-              <strong><i class="fas fa-book mr-1"></i> Education</strong>
-
-              <p class="text-muted">
-                B.S. in Computer Science from the University of Tennessee at Knoxville
-              </p>
-
+              <strong><i class="fas fa-signature mr-1"></i> Nama Lengkap</strong>
+              <p class="text-muted"><?= $user['nama_user'] ?></p>
               <hr>
-
-              <strong><i class="fas fa-map-marker-alt mr-1"></i> Location</strong>
-
-              <p class="text-muted">Malibu, California</p>
-
+              <strong><i class="fas fa-map-marker-alt mr-1"></i> Alamat</strong>
+              <p class="text-muted"><?= $user['alamat'] ?></p>
               <hr>
-
-              <strong><i class="fas fa-pencil-alt mr-1"></i> Skills</strong>
-
-              <p class="text-muted">
-                <span class="tag tag-danger">UI Design</span>
-                <span class="tag tag-success">Coding</span>
-                <span class="tag tag-info">Javascript</span>
-                <span class="tag tag-warning">PHP</span>
-                <span class="tag tag-primary">Node.js</span>
-              </p>
-
+              <strong><i class="fas fa-venus-mars mr-1"></i> Jenis Kelamin</strong>
+              <p class="text-muted"><?= $user['jenis_kelamin'] ?></p>
               <hr>
-
-              <strong><i class="far fa-file-alt mr-1"></i> Notes</strong>
-
-              <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam fermentum enim neque.</p>
             </div>
             <!-- /.card-body -->
           </div>
@@ -97,8 +63,8 @@
           <div class="card">
             <div class="card-header p-2">
               <ul class="nav nav-pills">
-                <li class="nav-item"><a class="nav-link active" href="#activity" data-toggle="tab">Activity</a></li>
-                <li class="nav-item"><a class="nav-link" href="#timeline" data-toggle="tab">Timeline</a></li>
+                <li class="nav-item"><a class="nav-link active" href="#activity" data-toggle="tab">Info Pribadi</a></li>
+                <li class="nav-item"><a class="nav-link" href="#timeline" data-toggle="tab">Kirim Pesan</a></li>
                 <li class="nav-item"><a class="nav-link" href="#settings" data-toggle="tab">Settings</a></li>
               </ul>
             </div><!-- /.card-header -->
